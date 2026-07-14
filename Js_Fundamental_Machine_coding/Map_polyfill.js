094575a643578b440ee5d4aa4c87d67886d87c1c
Madhav Pathak <73849357+code22madhav@users.noreply.github.com>
2026-07-14 19:59:25 +0530

    Update and rename Map polyfill.js to Map_polyfill.js

--- Js_Fundamental_Machine_coding/Map_polyfill.js
+++ Js_Fundamental_Machine_coding/Map_polyfill.js
@@ -23,20 +23,20 @@ const arr=[12,2,3,4]
 // console.log(arr)
 // console.log(map(arr,sqaure));
 
-Array.prototype.myMap=function (callback){
+Array.prototype.myMap=function (callback,thisArg){
     if(typeof callback !== "function"){
         throw new TypeError("callback must be a function");
     }
     const len=this.length
     const result=new Array(this.length)
     for(let i=0;i<len;i++){
-        if(this.hasOwnProperty(i)){
-            result[i]=callback(this[i],i,this)
+        if(Object.hasOwn(this,i)){
+            result[i]=callback.call(thisArg,this[i],i,this)
         }
     }
     return result;
 }
 
 console.log(arr)
-console.log(arr.myMap(sqaure));
+console.log(arr.myMap(sqaure,arr))
 
